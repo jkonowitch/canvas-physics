106f8f1920e6ca9a0392cc6dd7d99f7fa9597c47
Jeff Konowitch <jkonowitch@stridenyc.com>
2018-03-21 12:03:55 -0400

    collision physics! I actually did it!

--- src/canvas/core.cljs
+++ src/canvas/core.cljs
@@ -50,9 +50,10 @@
       (update :x + dx)
       (update :y + dy)))
 
-; (defn resolve-collision
-;   [circles]
-;   (map #(update :dx -) circles))
+(defn reverse-direction [c]
+  (-> c
+    (update :dx -)
+    (update :dy -)))
 
 (def move-with-wall-detection (comp resolve-wall-x resolve-wall-y  move))
 
@@ -79,13 +80,59 @@
    (.stroke ctx)
    (.fill ctx))
 
+(defn m-assoc [cs [i circle]]
+  (assoc cs i circle))
+
+(defn magnitude [v->]
+  (->> v->
+      (map #(.pow js/Math % 2))
+      (reduce +)
+      (.sqrt js/Math)))
+
+(defn dot-product [& vs->]
+  (reduce + (apply map * vs->)))
+
+(defn one-d-velocity [v1 m1 v2 m2]
+  (let [numerator (+ (* v1 (- m1 m2)) (* 2 m2 v2))
+        denominator (+ m1 m2)]
+    (/ numerator denominator)))
+
+(defn collide-physics [[[i c1] [j c2] :as tupe]]
+  (let [c1-> [(:x c1) (:y c1)]
+        c2-> [(:x c2) (:y c2)]
+        nrml-> (map - c1-> c2->)
+        un-> (map #(/ % (magnitude nrml->)) nrml->)
+        ut-> [(- (last un->)) (first un->)]
+        vc1-> [(:dx c1) (:dy c1)]
+        vc2-> [(:dx c2) (:dy c2)]
+        vc1n (dot-product un-> vc1->)
+        vc2n (dot-product un-> vc2->)
+        vc1t (dot-product ut-> vc1->)
+        vc2t (dot-product ut-> vc2->)
+        vc1n' (one-d-velocity vc1n (:radius c1) vc2n (:radius c2))
+        vc2n' (one-d-velocity vc2n (:radius c2) vc1n (:radius c1))
+        vc1n->' (map (partial * vc1n') un->)
+        vc2n->' (map (partial * vc2n') un->)
+        vc1t->' (map (partial * vc1t) ut->)
+        vc2t->' (map (partial * vc2t) ut->)
+        vc1->' (map + vc1n->' vc1t->')
+        vc2->' (map + vc2n->' vc2t->')
+        c1' (merge c1 (zipmap [:dx :dy] vc1->'))
+        c2' (merge c2 (zipmap [:dx :dy] vc2->'))]
+    [[i c1'][j c2']]))
+
+(def frame (.-requestAnimationFrame js/window))
+
 (defn animate! [circles]
   (.clearRect ctx 0 0 innerWidth innerHeight)
   (doseq [c circles] (draw! c))
-  (when-let [a (not-empty (colliding-tuples circles))] (println a))
-  (.requestAnimationFrame js/window (partial animate! (map move-with-wall-detection circles))))
+  (if-let [a (not-empty (colliding-tuples circles))]
+    (do
+      (let [b (apply concat (map collide-physics a))]
+        (frame (partial animate! (map move-with-wall-detection (reduce m-assoc (vec circles) b))))))
+    (frame (partial animate! (map move-with-wall-detection circles)))))
 
-(animate! (repeatedly 2 #(make-circle)))
+(animate! (repeatedly 5 #(make-circle)))
 
 ;(defn on-js-reload [])
   ;; optionally touch your app-state to force rerendering depending on
